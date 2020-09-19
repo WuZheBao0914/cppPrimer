124c8a63f475d76b97688b30c6efe44c38cb6c17
wuzhewei <627945683@qq.com>
2020-09-19 20:57:23 +0800

    新增文件夹

--- 1-3.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-	string str = "ÄãºÃ";
-	for (int i = 0; i < 4; i++) {
-		cout << str[i] << endl;
-	}
-	return 0;
-}
---
+++ Chapter1/1-3.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+	string str = "hello world";
+	cout << str;
+	return 0;
+}
